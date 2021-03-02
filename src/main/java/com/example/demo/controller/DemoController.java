ba8477eb86ee006b5afecb6aca50952cd0822010
Venkata kiran kumar Nemana <kiran.nvkk@gmail.com>
2021-03-02 16:32:24 +0530

    added property

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class DemoController {
 
-    @GetMapping("/{name}")
-    public String getGreetings(@PathVariable String name) {
+    @Value("${demo.name}")
+    private String name;
+
+    @GetMapping()
+    public String getGreetings() {
         return "Hello Openshit application demo after applying web hook in github from "+name;
     }
 }
